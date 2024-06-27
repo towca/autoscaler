0d34d484259b4269e55e5951bb4f436988143b26
Kuba Tużnik <jtuznik@google.com>
2024-06-27 19:17:04 +0200

    CA: remove unused CreateNodeNameToNodeInfoMap

--- cluster-autoscaler/utils/scheduler/scheduler.go
+++ cluster-autoscaler/utils/scheduler/scheduler.go
@@ -37,44 +37,6 @@ const (
 	schedulerConfigInvalidErr  = "invalid KubeSchedulerConfiguration"
 )
 
-// CreateNodeNameToInfoMap obtains a list of pods and pivots that list into a map where the keys are node names
-// and the values are the aggregated information for that node. Pods waiting lower priority pods preemption
-// (pod.Status.NominatedNodeName is set) are also added to list of pods for a node.
-func CreateNodeNameToInfoMap(pods []*apiv1.Pod, nodes []*apiv1.Node) map[string]*schedulerframework.NodeInfo {
-	nodeNameToNodeInfo := make(map[string]*schedulerframework.NodeInfo)
-	for _, pod := range pods {
-		nodeName := pod.Spec.NodeName
-		if nodeName == "" {
-			nodeName = pod.Status.NominatedNodeName
-		}
-		if _, ok := nodeNameToNodeInfo[nodeName]; !ok {
-			nodeNameToNodeInfo[nodeName] = schedulerframework.NewNodeInfo()
-		}
-		nodeNameToNodeInfo[nodeName].AddPod(pod)
-	}
-
-	for _, node := range nodes {
-		if _, ok := nodeNameToNodeInfo[node.Name]; !ok {
-			nodeNameToNodeInfo[node.Name] = schedulerframework.NewNodeInfo()
-		}
-		nodeNameToNodeInfo[node.Name].SetNode(node)
-		// TODO(DRA): nodeNameToNodeInfo[node.Name].SetDynamicResources(???)
-	}
-
-	// Some pods may be out of sync with node lists. Removing incomplete node infos.
-	keysToRemove := make([]string, 0)
-	for key, nodeInfo := range nodeNameToNodeInfo {
-		if nodeInfo.Node() == nil {
-			keysToRemove = append(keysToRemove, key)
-		}
-	}
-	for _, key := range keysToRemove {
-		delete(nodeNameToNodeInfo, key)
-	}
-
-	return nodeNameToNodeInfo
-}
-
 func isHugePageResourceName(name apiv1.ResourceName) bool {
 	return strings.HasPrefix(string(name), apiv1.ResourceHugePagesPrefix)
 }
