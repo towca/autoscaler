202899b5634155f9467364b712969f2dc5c29ef5
Kuba Tużnik <jtuznik@google.com>
2024-08-28 16:44:35 +0200

    DRA: Embed Pod/Node inside the ResourceInfo structs

--- cluster-autoscaler/simulator/clustersnapshot/clustersnapshot.go
+++ cluster-autoscaler/simulator/clustersnapshot/clustersnapshot.go
@@ -27,13 +27,13 @@ import (
 
 // NodeResourceInfo contains all information about a Node and its associated resources needed by the scheduler.
 type NodeResourceInfo struct {
-	Node             *apiv1.Node
+	*apiv1.Node
 	DynamicResources schedulerframework.NodeDynamicResources
 }
 
 // PodResourceInfo contains all information about a Pod and its associated resource requests needed by the scheduler.
 type PodResourceInfo struct {
-	Pod                     *apiv1.Pod
+	*apiv1.Pod
 	DynamicResourceRequests schedulerframework.PodDynamicResourceRequests
 }
 
